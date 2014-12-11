b3edd5ddef3c9d256d7f19b86a14e89018599d51
unknown <irahavoi@gmail.com>
2014-12-11 15:56:51 -0600

    added the router. + listening to remove and toggle events

---
+++ todoApp/js/routers/router.js
@@ -0,0 +1,16 @@
+// js/routers/router.js
+
+//Todo router
+
+var Workspace = Backbone.Router.extend({
+	routes : {
+		'*filter' : 'setFilter'
+	},
+
+	setFilter : function(param){
+		window.app.Todos.trigger('filter');
+	}
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
--- todoApp/js/views/todos.js
+++ todoApp/js/views/todos.js
@@ -16,6 +16,8 @@ app.TodoView = Backbone.View.extend({
 	//The dom events specific to a todo item
 	events: {
 		'dblclick label': 'edit',
+		'click .toggle' : 'toggleCompleted',
+		'click .destroy' : 'clear',
 		'keypress .edit': 'updateOnEnter',
 		'blur .edit': 'close'
 	},
@@ -25,13 +27,36 @@ app.TodoView = Backbone.View.extend({
 	// app, we set a direct reference on the model for convenience.
 	initialize: function() {
 	 	this.listenTo(this.model, 'change', this.render);
+	 	this.listenTo(this.model, 'destroy', this.remove);
+	 	this.listenTo(this.model, 'visible', this.toggleVisible);
 	},
 	 // Rerenders the titles of the todo item.
 	 render: function() {
 	 	this.$el.html( this.template( this.model.toJSON() ) );
 	 	this.$input = this.$('.edit');
+	 	this.$el.toggleClass( 'completed', this.model.get('completed') );
+ 		this.toggleVisible();
 	 	return this;
 	 },
+
+	 toggleVisible : function(){
+	 	console.log('toggle visible')
+	 	this.$el.toggleClass('visible' , this.model.get('completed'));
+	 },
+
+
+	 isHidden : function(){
+	 	var isCompleted = this.model.get('completed');
+	 	return((!isCompleted && app.TodoFilter === 'completed') ||
+	 			(isCompleted && app.TodoFilter === 'active')
+	 		);
+	 },
+
+	 toggleCompleted : function(){
+	 	console.log('toggle');
+	 	this.model.toggle();
+	 },
+
 	 // Switch this view into `"editing"` mode, displaying the input field.
 	 edit: function() {
 	 	this.$el.addClass('editing');
@@ -50,5 +75,12 @@ app.TodoView = Backbone.View.extend({
 	 	if ( e.which === ENTER_KEY ) {
 	 		this.close();
 	 	}
+	 },
+
+	 // Remove the item, destroy the model from
+	 // *localStorage* and delete its view.
+	 clear: function() {
+	 console.log('remove');
+	 this.model.destroy();
 	 }
 });
