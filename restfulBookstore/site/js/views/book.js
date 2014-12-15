17a6c1593b1c8573664c32a0b815e9ec49cce79f
unknown <irahavoi@gmail.com>
2014-12-14 21:26:57 -0600

    Deleting the book model and views

--- restfulBookstore/site/js/views/book.js
+++ restfulBookstore/site/js/views/book.js
@@ -7,10 +7,21 @@ app.BookView = Backbone.View.extend({
 	className : 'bookContainer',
 	template : _.template($('#bookTemplate').html()),
 
+	events  : {
+		'click .delete' : 'deleteBook'
+	},
+
 	render : function(){
 		console.log('rendering a book');
 		this.$el.html(this.template(this.model.toJSON()));
 
 		return this;
+	}, 
+
+	deleteBook : function(){
+		console.log('deleting a book..');
+		this.model.destroy(); //delete model
+		this.remove(); //remove view
 	}
+
 });
