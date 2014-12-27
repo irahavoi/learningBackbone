a7713acce31efa627f8acaddbf7957b80a43ee1b
unknown <irahavoi@gmail.com>
2014-12-26 23:07:56 -0600

    thorax model example

--- thoraxExamples/hello/js/main.js
+++ thoraxExamples/hello/js/main.js
@@ -7,9 +7,10 @@ require([
 ], function ($, Backbone, RootView, HelloWorldRouter) {
   $(function() {
   var view = new Thorax.View({
+    model : new Thorax.Model({firstname : 'Illia', lastname  : 'Rahavoi'}),
     hi: "Hello",
     world : 'World',
-    template: Handlebars.compile("{{hi}} {{world}}")
+    template: Handlebars.compile("Hello, Mr. {{firstname}} {{lastname}}")
   });
   view.appendTo('body');
   });
