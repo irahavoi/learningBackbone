2c98b5b2600d57a279cfd5d0ec5cd29771d02066
unknown <irahavoi@gmail.com>
2014-12-27 22:00:11 -0600

    embedding child views

--- thoraxExamples/hello/js/main.js
+++ thoraxExamples/hello/js/main.js
@@ -6,11 +6,23 @@ require([
   'helpers'
 ], function ($, Backbone, RootView, HelloWorldRouter) {
   $(function() {
+
+  var childView1  = new Thorax.View({
+    template : Handlebars.compile('<li>I am a child view 1.</li>')
+  });
+
+  var childView2  = new Thorax.View({
+    template : Handlebars.compile('<li>I am a child view 2.</li>')
+  });
+
+
   var view = new Thorax.View({
     model : new Thorax.Model({firstname : 'Illia', lastname  : 'Rahavoi'}),
     hi: "Hello",
     world : 'World',
-    template: Handlebars.compile("Hello, Mr. {{firstname}} {{lastname}}")
+    child1 : childView1,
+    child2 : childView2,
+    template: Handlebars.compile("Hello, Mr. {{firstname}} {{lastname}} <br/><ul>{{view child1}}{{view child2}}</ul>")
   });
   view.appendTo('body');
   });
