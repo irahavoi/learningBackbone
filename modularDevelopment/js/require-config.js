cea17004aa2a7748297e9bfdb40b31c2a1561307
unknown <irahavoi@gmail.com>
2014-12-29 22:48:27 -0600

    backbone and dependencies loading using require.js

--- modularDevelopment/js/require-config.js
+++ modularDevelopment/js/require-config.js
@@ -6,13 +6,21 @@ require.config({
 	},
 	shim: {
 		'backbone': {
+			//deps lists dependencies (anderscore and jquery) that have to be loaded before Backbone.js
 			deps : ['underscore', 'jquery'],
+			// Backbone does not directly support amd, so we have to specify, which global variable is going to be expported.
 			exports: 'Backbone'
 		}
 	}
 });
 
 require( ['backbone'] , function(backbone){
-	console.log('done loading backbone');
+	console.log('done loading backbone: ');
+	console.log(backbone);
+	console.log('As well as its\' dependencies:');
+	console.log('Underscore:');
+	console.log(_);
+	console.log('And jquery');
+	console.log(jQuery);
 });
 
