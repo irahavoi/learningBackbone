ed455f1fba99d568c78b5bef727f7b7961290d4d
unknown <irahavoi@gmail.com>
2015-01-01 17:52:54 -0600

    Basic require js config + template files

--- requirejsExampleApp/js/main.js
+++ requirejsExampleApp/js/main.js
@@ -1,1 +1,13 @@
-//TODO
+require.config({
+	baseUrl : '../',
+	paths : {
+		jquery : 'libs/jquery/jquery.min',
+		underscore : 'libs/underscore/underscore.min',
+		backbone : 'libs/backbone/backbone.min',
+		text: 'libs/require/text'
+	}
+});
+
+require(['views/app'], function(AppView){
+	var app_view = new AppView();
+});
