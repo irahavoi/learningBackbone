c99ee16d681622a98007f4cc73512786676a3065
unknown <irahavoi@gmail.com>
2015-01-01 19:30:45 -0600

    up

--- requirejsExampleApp/js/collections/todos.js
+++ requirejsExampleApp/js/collections/todos.js
@@ -1,1 +1,18 @@
-//TODO
+// js/collections/todos.js
+define(['underscore',
+		'backbone',
+		'localStorage',
+		'models/todo'
+		], function(_, Backbone, Store, Todo){
+			var TodoCollection = Backbone.collection.extend({
+				model : Todo,
+				localStorage : new Store('todos'),
+				done : function(){
+					return this.filter(function(todo){return todo.get('done');})
+				},
+
+				remaining : function(){
+					return this.without.apply(this, this.done())	
+				}
+			});
+		});
--- requirejsExampleApp/js/views/app.js
+++ requirejsExampleApp/js/views/app.js
@@ -1,1 +1,9 @@
-//TODO
+// js/views/app.js
+define(['jquery',
+		'underscore',
+		'backbone',
+		'collections/todos',
+		'views/todo',
+		'text!templates/stats.html'], function($, _, Backbone, Todos, TodoView, statsTemplate){
+			
+		});
