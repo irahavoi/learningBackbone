beba36fc56c2f7294c8ca6275b0b06e3b7951e2a
unknown <irahavoi@gmail.com>
2015-01-01 18:41:20 -0600

    localstorage plug-in

--- requirejsExampleApp/js/main.js
+++ requirejsExampleApp/js/main.js
@@ -4,6 +4,7 @@ require.config({
 		jquery : 'libs/jquery/jquery.min',
 		underscore : 'libs/underscore/underscore.min',
 		backbone : 'libs/backbone/backbone.min',
+		localStorage : 'libs/backbone/backbone.localStorage-min',
 		text: 'libs/require/text'
 	}
 });
