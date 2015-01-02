84fff9b4b1d882d41fa1232c22711847393e387a
unknown <irahavoi@gmail.com>
2015-01-01 18:38:19 -0600

    todo model

--- requirejsExampleApp/js/models/todo.js
+++ requirejsExampleApp/js/models/todo.js
@@ -1,1 +1,30 @@
-//TODO
+// js/models/todo.js
+
+define(['underscore', 'backbone'], function(_, Backbone){
+	var TodoModel = Backbone.extend.Model({
+		//Default attributes for the Todo:
+		defaults : {
+			//Default content:
+			content : 'empty todo...',
+			done : 'false'
+		},
+
+		initialize : function(){
+			console.log('initializing TodoModel..');
+		},
+
+		//Toggle the done state of the given todo item:
+		toggle : function(){
+			this.save({done : !this.get('done')})
+		},
+
+		//Remove this todo from the local storage and delete it's view:
+		clear : function(){
+			this.destroy();
+			this.view.remove();
+		}
+
+	});
+
+	return TodoModel;
+});
