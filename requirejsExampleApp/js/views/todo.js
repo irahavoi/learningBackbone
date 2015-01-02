3fef18765fa8df00e368a80e58c98db700d084ac
unknown <irahavoi@gmail.com>
2015-01-01 19:44:26 -0600

    up

--- requirejsExampleApp/js/views/todo.js
+++ requirejsExampleApp/js/views/todo.js
@@ -42,16 +42,43 @@ define([
 			this.input.val(content);
 		},
 
-		edit : function(){
-			console.log('todo: edit');
+		// Switch this view into `"editing"` mode, displaying the input field.
+		edit: function () {
+			this.$el.addClass('editing');
+			this.$input.focus();
 		},
 
-		clear : function(){
-			console.log('todo: clear');
+		// Close the `"editing"` mode, saving changes to the todo.
+		close: function () {
+			var value = this.$input.val();
+			var trimmedValue = value.trim();
+
+			if (trimmedValue) {
+				this.model.save({ title: trimmedValue });
+
+				if (value !== trimmedValue) {
+					// Model values changes consisting of whitespaces only are not causing change to be triggered
+					// Therefore we've to compare untrimmed version with a trimmed one to chech whether anything changed
+					// And if yes, we've to trigger change event ourselves
+					this.model.trigger('change');
+				}
+			} else {
+				this.clear();
+			}
+
+			this.$el.removeClass('editing');
 		},
 
-		updateOnEnter : function(){
-			console.log('todo : updateOnEnter');
+		// If you hit `enter`, we're through editing the item.
+		updateOnEnter: function (e) {
+			if (e.keyCode === 13) {
+				this.close();
+			}
+		},
+
+		// Remove the item, destroy the model from *localStorage* and delete its view.
+		clear: function () {
+			this.model.destroy();
 		}
 
 	});
