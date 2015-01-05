c60914f58aa28cd4da10fa5c21980b3384434b9d
unknown <irahavoi@gmail.com>
2015-01-05 13:14:45 -0600

    wish view

---
+++ wishlistApp/js/views/wish.js
@@ -0,0 +1,73 @@
+// /js/views/wish.js
+
+define(['jquery',
+	'underscore',
+	'backbone',
+	'text!templates/wish.html'
+	], function($,_,Backbone,tmpl){
+		var WishView = Backbone.View.extend({
+		//Cache the template function for a single item:
+		template : _.template(tmpl),
+
+		//DOM events specific to an item:
+		events : {
+			'click .toggle' : 'toggleDone',
+			'click button.destroy' : 'clear',
+			'keypress .edit' : 'updateOnEnter'
+		},
+
+		initialize : function(){
+			console.log('initializing..');
+			this.model.on('change', this.render, this);
+			this.model.view = this;
+		},
+
+		//Render the contents of the todo item:
+		render  : function(){
+			console.log('rendering..');
+			this.$el.html(this.template(this.model.toJSON()));
+			this.setContent();
+			return this;
+		},
+
+		// Close the `"editing"` mode, saving changes to the todo.
+		close: function () {
+			var value = this.$input.val();
+			var trimmedValue = value.trim();
+
+			if (trimmedValue) {
+				this.model.save({ title: trimmedValue });
+
+				if (value !== trimmedValue) {
+					// Model values changes consisting of whitespaces only are not causing change to be triggered
+					// Therefore we've to compare untrimmed version with a trimmed one to chech whether anything changed
+					// And if yes, we've to trigger change event ourselves
+					this.model.trigger('change');
+				}
+			} else {
+				this.clear();
+			}
+
+			this.$el.removeClass('editing');
+		},
+
+		toggleCompleted : function(){
+			console.log('toggle');
+			this.model.toggle();
+		},
+
+		// If you hit `enter`, we're through editing the item.
+		updateOnEnter: function (e) {
+			if (e.keyCode === 13) {
+				this.close();
+			}
+		},
+
+		// Remove the item, destroy the model from *localStorage* and delete its view.
+		clear: function () {
+			this.model.destroy();
+		}
+	});
+
+return WishView;
+});
