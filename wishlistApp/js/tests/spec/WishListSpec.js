fa8ef6b9a64b957eec0884b7581e65f57de271cd
unknown <irahavoi@gmail.com>
2015-01-13 19:01:15 -0600

    more tests for wishlist collection

--- wishlistApp/js/tests/spec/WishListSpec.js
+++ wishlistApp/js/tests/spec/WishListSpec.js
@@ -4,6 +4,7 @@ describe("Wishlist", function() {
   it("should load the AMD module", function(done) {
     require(['models/wish', 'collections/wishes'], function (Wish, WishList) {
       expect(Wish).toBeDefined();
+      expect(WishList).toBeDefined();
       wish = new Wish();
       wishlist = WishList;	     
       done();
@@ -14,6 +15,13 @@ describe("Wishlist", function() {
     expect(wishlist.length).toBe(0);
     wishlist.add(wish);
     expect(wishlist.length).toBe(1);
+  });
+
+  it("Can add wishes as an array", function() {
+    var length = wishlist.length;
+
+    wishlist.add([{granted : true, content : 'Test 1'},{granted : false, content : 'Test 2'}]);
+    expect(wishlist.length).toBe(length + 2);
   });
  
 });
