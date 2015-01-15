160363252ab0295d08df1502f362e9fd47931322
unknown <irahavoi@gmail.com>
2015-01-15 16:12:14 -0600

    using module() to structure qunit tests

--- qunit/tests.js
+++ qunit/tests.js
@@ -3,3 +3,14 @@ var result = reverseString('backbone+qunit');
 // equal( actualValue, expectedValue, message )
 equal( result, 'tinuq+enobkcab', 'Expected to get a reversed string.');
 });
+
+
+module( 'Module One' );
+test( 'first test', function() {} );
+test( 'another test', function() {} );
+module( 'Module Two' );
+test( 'second test', function() {} );
+test( 'another test', function() {} );
+module( 'Module Three' );
+test( 'third test', function() {} );
+test( 'another test', function() {} );
