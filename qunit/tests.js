54e87b70a3872c993eee2be76448cde6a0a3bff5
unknown <irahavoi@gmail.com>
2015-01-17 20:19:27 -0600

    testing backbone with qunit

--- qunit/tests.js
+++ qunit/tests.js
@@ -1,36 +1,27 @@
-test( 'Testing the reverseString function', function(){
-var result = reverseString('backbone+qunit');
-// equal( actualValue, expectedValue, message )
-equal( result, 'tinuq+enobkcab', 'Expected to get a reversed string.');
+// Define a simple model and collection modeling a store and
+// list of stores
+var Store = Backbone.Model.extend({});
+var StoreList = Backbone.Collection.extend({
+  model: Store,
+  comparator: function( Store ) { return Store.get('name') }
 });
-
-
-module( 'Module One' , {
-  setup : function(){
-    console.log('setting up module one..');
+// Define a group for our tests
+module( 'StoreList sanity check', {
+  setup: function() {
+    this.list = new StoreList;
+    this.list.add(new Store({ name: 'Costcutter' }));
+    this.list.add(new Store({ name: 'Target' }));
+    this.list.add(new Store({ name: 'Walmart' }));
+    this.list.add(new Store({ name: 'Barnes & Noble' }));
   },
-  teardown : function(){
-    console.log('Tearing down module one..')
+  teardown: function() {
+    window.errors = null;
   }
 });
-
-test( 'first test', function() {
-  expect(0);
-} );
-test( 'another test', function() {
-  expect(0);
-} );
-module( 'Module Two' );
-test( 'second test', function() {
-  expect(0);
-} );
-test( 'another test', function() {
-  expect(0);
-} );
-module( 'Module Three' );
-test( 'third test', function() {
-  expect(0);
-} );
-test( 'another test', function() {
-  expect(0);
-} );
+// Test the order of items added
+test( 'test ordering', function() {
+  expect( 1 );
+  var expected = ['Barnes & Noble', 'Costcutter', 'Target', 'Walmart'];
+  var actual = this.list.pluck('name');
+  deepEqual( actual, expected, 'is maintained by comparator' );
+});
